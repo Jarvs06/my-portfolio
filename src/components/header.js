192a9c9b91c89ec7823b4dea6b22343d225e32c6
jarveyilano <jarvsgithub@gmail.com>
2025-11-06 11:28:32 +0800

    Added Navigation Bar

---
+++ src/components/header.js
@@ -0,0 +1,24 @@
+import { Link } from "react-scroll";
+
+function Header() {
+  return (
+    <section id="Header">
+            <div id='navigationBar'>
+                <div id='navigationContainer'>
+                    <div id='logo'><a href="/">Jarvey<span>.dev</span></a></div>
+                    <div id='navigationMenu'>
+                        <ul>
+                            <li><Link to="homeBanner" smooth={true} duration={500}>Home</Link></li>
+                            <li><Link to="homeAbout" smooth={true} duration={500}>About</Link></li>
+                            <li><Link to="homeProjects" smooth={true} duration={500}>Projects</Link></li>
+                            <li><Link to="homeSkills" smooth={true} duration={500}>Skills</Link></li>
+                            <li><Link to="homeContact" smooth={true} duration={500}>Let's Talk</Link></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Header;
---
+++ src/pages/home.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <>
+      <section id='homeBanner'>
+        <div id='bannerContainer'>
+          <h1>Hi</h1>
+        </div>
+      </section>
+
+
+      <section id='homeAbout'>
+
+      </section>
+
+
+      <section id='homeProjects'>
+
+      </section>
+
+
+      <section id='homeSkills'>
+
+      </section>
+
+
+      <section id='homeContact'>
+
+      </section>
+    </>
+  );
+}
+export default Home;
